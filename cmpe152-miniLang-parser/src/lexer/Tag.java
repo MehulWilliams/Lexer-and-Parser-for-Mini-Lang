9a07d872e01cedde86ddfa6d9692c57a8accb345
Mehul Williams <mehulwilliams@gmail.com>
2021-11-07 23:30:17 -0800

    Update Tag.java

--- cmpe152-miniLang-parser/src/lexer/Tag.java
+++ cmpe152-miniLang-parser/src/lexer/Tag.java
@@ -4,11 +4,12 @@ public class Tag {
 
     public final static int
             AND = 1, BASIC = 2, BREAK = 3, DO = 4, ELSE = 5,
-            EQ = 6, FALSE = 7,  FOR = 8, GE = 9, ID = 10, IF = 11,
+            EQ = 6, FALSE = 7, FOR = 8, GE = 9, ID = 10, IF = 11,
             LE = 12, NE = 13, NUM = 14,
             OR = 15, REAL = 16, TRUE = 17, WHILE = 18,
+            INCR = 32, DECR = 33,
 
-            EOF = 31;
+    EOF = 31;
 
 
 }
